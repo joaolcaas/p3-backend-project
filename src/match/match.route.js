0e50ddd2ad2bdb37af6fe87e88c843e36983ef08
JOAO LUCAS ARAUJO FELIPE 114211023 <joaolaf@lcc3-038.lcc.ufcg.edu.br>
2018-09-19 14:35:59 -0300

    aula de quarta

--- src/match/match.route.js
+++ src/match/match.route.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = new express.Router();
-
+const user_util = require('../util/user.util')
 const users = require('../data/user.json');
 
 router.use((req,res,next) => {
     next();
 });
 
-/**
- * return all matchs
- */
+
 router.get('/',function(req,res){
     const users_game = []
     users.forEach(function(element){
@@ -17,4 +15,16 @@ router.get('/',function(req,res){
     });
     res.send(users_game)
 });
+
+router.get('/:id1/:id2',function(req,res){
+    const user1 = user_util.findUser(users,req.params.id1) 
+    const user2 = user_util.findUser(users,req.params.id2)
+
+    if(user1.interest_games[0].game[0] == user2.interest_games[0].game[0] && user1.interest_games[0].data[0] == user2.interest_games[0].data[0]){
+        
+    }
+
+
+ });
+
 module.exports = router;
