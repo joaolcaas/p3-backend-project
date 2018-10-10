67dbc0b3d53f4f31bdc39a41c772d6ea67866fee
JOAO LUCAS ARAUJO FELIPE 114211023 <joaolaf@lcc3-043.lcc.ufcg.edu.br>
2018-10-10 14:32:19 -0300

    interest controller

--- src/interest/interest.route.js
+++ src/interest/interest.route.js
@@ -1,68 +1,10 @@
 const express = require('express');
 const router = new express.Router();
-const modelUser = require('../user/user.model');
 const auth = require('../auth/auth.service');
-router.use((req,res,next) => {
-    next();
-});
+const controller = require('./interest.controller');
 
-router.get('/:id',auth.ensureAuthenticated, auth.authenticateById,function(req,res){
-    const user_id = req.params.id;
+router.get('/:id',auth.ensureAuthenticated, auth.authenticateById,controller.getinterst);
 
-    modelUser.findOne({'id':user_id}).then((user,err)=>{
-        if(err || user == null){
-            return res.status(400).send('usuario não encontrado')
-        }else{
-            if(user.interest_game != null){
-                return res.status(200).send(user.interest_game)
-            }else{
-                return res.status(400).send('voce não tem jogos de interesse')
-            }
-        }
-    })
-    
-})
-
-router.post('/:id',auth.ensureAuthenticated, auth.authenticateById,function(req,res){
-    const user_id = req.params.id;
-    const game = req.query.game;
-    const data = req.query.data;
-
-    modelUser.findOne({'id':user_id}).then((user,err)=>{
-        if(err || user == null){
-            return res.status(400).send('usuario não encontrado')
-        }else{
-            if(user.interest_game.get(game)){
-                if(user.interest_game.get(game).includes(data)){
-                    res.status(400).send('esse horário de jogo já está marcado')
-                }else{
-                    user.interest_game.get(game).push(data);
-                    user.save((error)=>{
-                        if(error){
-                            const message = error.errmsg || error.message;
-                            res.status(400).send(message)
-                        }else{
-                            res.status(201).send('Atualização feita com sucesso')
-                        }
-                    
-                });
-            }
-            }
-            else{
-                user.interest_game.set(game,[data])
-                user.save((error)=>{
-                    if(error){
-                        const message = error.errmsg || error.message;
-                        res.status(400).send(message)
-                    }else{
-                        res.status(201).send('Atualização feita com sucesso')
-                    }
-                    
-                });
-            }        
-        }
-    })     
-
-})
+router.post('/:id',auth.ensureAuthenticated,auth.authenticateById,controller.postinterest);
 
 module.exports = router;
